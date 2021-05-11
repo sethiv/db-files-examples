dd47a6ab4dc9ffa4fad4e7bd30affba3b63c4968
Vaibhav Sethi <vaibsethi@gmail.com>
2021-05-11 10:18:12 -0700

    Delete test.py

--- Migration/RunCommandsToFiles/withFiles/Modules/test.py
+++
@@ -1,6 +0,0 @@
-def continousToBinary(data, column_name):
-  average = data[column_name].mean()
-  binary_column_name = "binary_"+column_name
-  binary_column =(data[column_name] >= average).astype(int)
-  data[binary_column_name] = binary_column
-  return data
